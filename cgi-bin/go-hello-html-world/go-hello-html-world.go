6846854fff7b24e2ba5935df9aeeb8a9eb5dddbd
wwidjaja0 <me@wwidjaja.com>
2025-08-19 15:25:04 -0700

    Test hello.go

--- cgi-bin/go-hello-html-world/go-hello-html-world.go
+++ cgi-bin/go-hello-html-world/go-hello-html-world.go
@@ -4,16 +4,25 @@ import (
 	"fmt"
 	"net/http"
 	"net/http/cgi"
+	"os"
+	"time"
 )
 
 func main() {
 	cgi.Serve(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Content-type", "text/html")
+
+		now := time.Now()
+		ip := os.Getenv("REMOTE_ADDR")
+
 		fmt.Fprint(w, "<html><body>")
-		fmt.Fprint(w, "<h1>Hello from Go CGI!</h1>")
-		if err := r.ParseForm(); err == nil {
-			fmt.Fprintf(w, "<p>Received data: %v</p>", r.Form)
-		}
+		fmt.Fprint(w, "<h1>Hello, Go!</h1>")
+		fmt.Fprint(w, "<p style=\"background-color: yellow;\">William Widjaja</p>")
+		fmt.Fprint(w, "<br />")
+		fmt.Fprint(w, "<p>This page was generated with the Go programming langauge</p>")
+		fmt.Fprintf(w, "Current Time: %s", now)
+		fmt.Fprintf(w, "Your IP Address: %s", ip)
 		fmt.Fprint(w, "</body></html>")
 	}))
 }
