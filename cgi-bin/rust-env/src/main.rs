41da4b8dcd90c2fbe06d76eac09d583dbc2798c4
wwidjaja0 <contact@wwidjaja.com>
2025-08-19 12:08:49 -0700

    Rust env formatting

--- cgi-bin/rust-env/src/main.rs
+++ cgi-bin/rust-env/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
     println!("<br />");
 
     for (key, value) in env::vars() {
-        println!("{}: {}", key, value);
+        println!("{}: {}<br />", key, value);
     }
 
     println!("</body></html>");
