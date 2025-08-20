3bcd7526039a3c9a3d379de3ab9c5f2f71e937aa
wwidjaja0 <me@wwidjaja.com>
2025-08-19 17:19:42 -0700

    Fix destroy session link

--- cgi-bin/rust-destroy-session/src/main.rs
+++ cgi-bin/rust-destroy-session/src/main.rs
@@ -11,7 +11,7 @@ fn main() {
     println!("<body>");
     println!("<h1>Rust Session Destroyed</h1>");
 
-    println!("<a href=\"/cgi-bin/rust-sessions.cgi\">Back to Page 1</a><br/>");
+    println!("<a href=\"/cgi-bin/rust-session.cgi\">Back to Page 1</a><br/>");
     println!("<a href=\"/hw2/rust-cgiform.html\">Rust CGI Form</a><br/>");
     println!("<p style=\"background-color: yellow;\">William Widjaja</p>");
 
